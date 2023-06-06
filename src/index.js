21201cade43968eed8928be4879d41b876b6451e
Vinyas <svinyas2000@gmail.com>
2023-06-06 14:09:01 +0530

    completed consumption and sening main

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const express = require('express')
-
+const amqplib = require('amqplib')
+const {EmailService} =require('./services')
+async function connectQueue(){
+try {
+    const connection = await amqplib.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+    await channel.assertQueue("noti-queue");
+    channel.consume("noti-queue",async (data)=>{
+        const object = JSON.parse(`${Buffer.from(data.content)}`);
+        await EmailService.sendEmail("vinyassatheesh@gmail.com",object.recepientEmail, object.subject, object.text)
+        channel.ack(data)
+    })
+} catch (error) {
+    
+}
+}
 const {ServerConfig, Logger} =require('./config')
 const apiRoutes = require('./routes')
 const app =express();
@@ -13,5 +28,6 @@ app.use('/api',apiRoutes);
 app.listen(ServerConfig.PORT , async () => {
     console.log(`server started on ${ServerConfig.PORT}`);
     Logger.info("Sucessfully started server ",{});
-    
+    await connectQueue();
+    console.log("queue is up")
 });
